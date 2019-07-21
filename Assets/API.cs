d150d53f3571a4df045ac9f2fa9a07fb1aa74e5e
GehadAbdElhalim <halimgego@gmail.com>
2019-07-21 15:35:56 +0200

    first commit

---
+++ Assets/API.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Networking;
+
+public class API : MonoBehaviour
+{
+    public TextMeshProUGUI output;
+
+    public void test()
+    {
+        output.text = "Pressed";
+        StartCoroutine("GetText");
+    }
+
+    IEnumerator GetText()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get("https://www.google.com"))
+        {
+            output.text = "do anything please";
+            yield return www.SendWebRequest();
+
+            output.text = "what happened";
+
+            //if(www.isNetworkError || www.isHttpError)
+            //{
+                //Debug.Log(www.error);
+            //    output.text = "whyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy";
+            //}
+            //else
+            //{
+                output.text = www.downloadHandler.text;
+            //}
+        }
+    }
+}
---
+++ Assets/API2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class API2 : MonoBehaviour
+{
+    private const string URL = "www.google.com";
+    public TextMeshProUGUI output;
+
+    public void test()
+    {
+        WWW req = new WWW(URL);
+        StartCoroutine(OnResponse(req));
+    }
+
+    IEnumerator OnResponse(WWW req)
+    {
+        yield return req;
+
+        output.text = req.text;
+    }
+}
